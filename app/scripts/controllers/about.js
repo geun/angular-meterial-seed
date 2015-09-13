c4ecf453d98cca713415b53486ce6f6b436d5371
geun <geun@chattingcat.com>
2015-09-13 17:17:50 +0900

    initialize app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name plipApiAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the plipApiAdminApp
+ */
+angular.module('plipApiAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name plipApiAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the plipApiAdminApp
+ */
+angular.module('plipApiAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
